4b8f534b9ffe2e2ea2dff7af1f54f948201e7ab2
EDGAR HERNANDEZ GARCIA <iscehg@gmail.com>
2016-09-30 10:50:02 -0500

    Operaciones de Pitagoras

---
+++ Teorema de Pitagoras/operaciones.js
@@ -0,0 +1,28 @@
+function TeoremaPitagoras(a, b, c){
+	this.a = a;
+	this.b = b;
+	this.c = c;
+
+	this.CalcularAngulo = function(){
+		var a1 = this.a;
+		return true;
+	};
+
+	this.calcularCatetoA = function(){
+		var catetoA = Math.sqrt(Math.pow(this.c, 2) - Math.pow(this.b, 2));
+		return catetoA;
+	};
+
+	this.CalcularCatetoB = function(){
+		var catetoB = Math.sqrt(Math.pow(this.c, 2) - Math.pow(this.a, 2));
+		return catetoB;
+	};
+	this.CalcularHipotenusa = function(){
+		var hipotenusa = Math.sqrt(Math.pow(this.a, 2) + Math.pow(this.b, 2));
+		return hipotenusa;
+	};
+	
+	return this;
+}
+
+module.exports.TeoremaPitagoras = TeoremaPitagoras;
